ca8548d48e04d4fef6a3f03429233535f8c1df52
Briun Greene <harmonickarma789@gmail.com>
2026-02-18 03:33:18 -0500

    Completed car data exercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Dynamic;
+
+namespace CarLotSimulator;
+
+public class Car
+{
+    public int Year { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string EngineNoise { get; set; }
+    public string HonkNoise { get; set; }
+    public bool IsDriveable { get; set; }
+
+    public void MakeEngineNoise()
+    {
+        Console.WriteLine($"The engine makes a {EngineNoise} sound");
+    }
+
+    public void MakeHonkNoise()
+    {
+        Console.WriteLine($"The horn makes a {HonkNoise} sound");
+    }
+
+    public void ShowData()
+    {
+        Console.WriteLine($"This car is a {Year}-{Make}-{Model}- Driving condition is {IsDriveable}");
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -12,8 +12,41 @@ namespace CarLotSimulator
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
+            var car1 = new Car();
+            car1.Year = 2012;
+            car1.Make = "Volvo";
+            car1.Model = "XC90";
+            car1.EngineNoise = "vrrrrmmm";
+            car1.HonkNoise = "onk onk";
+            car1.IsDriveable = true;
 
-
+            var car2 = new Car();
+            car2.Year = 2018;
+            car2.Make = "Audi";
+            car2.Model = "SQ7";
+            car2.EngineNoise = "yrrrnnn";
+            car2.HonkNoise = "onk onk";
+            car2.IsDriveable = true;
+            
+            var car3 = new Car();
+            car3.Year = 2022;
+            car3.Make = "Dodge";
+            car3.Model = "Dakota";
+            car3.EngineNoise = "clunk clunk";
+            car3.HonkNoise = "beep beep";
+            car3.IsDriveable = false;
+            
+            car1.ShowData();
+            car1.MakeEngineNoise();
+            car1.MakeHonkNoise();
+            
+            car2.ShowData();
+            car2.MakeEngineNoise();
+            car2.MakeHonkNoise();
+            
+            car3.ShowData();
+            car3.MakeEngineNoise();
+            car3.MakeHonkNoise();
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
